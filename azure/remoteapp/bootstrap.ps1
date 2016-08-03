3c7d371e94e7d316a720749ead50bc3f8c038e8c
Philip <phucvd.ce@gmail.com>
2016-08-03 15:53:49 +0700

    Create bootstrap.ps1

---
+++ azure/remoteapp/bootstrap.ps1
@@ -0,0 +1,14 @@
+#----------------------------------
+#       bootstrap.ps1
+#       Created by Philip Van
+#       Aug 03 2016
+#----------------------------------
+#       Creating a RemoteApp Collection on Azure
+#      
+#----------------------------------
+$admin_acc = ""
+$Colname = ""
+$ImageName = ""
+$VNetName = ""
+$SubnetName = ""
+$Domain = ""
